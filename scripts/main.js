ef540873b63d58b0e0e6d544fc9e9b3ab7541db4
skyneton <67633420+skyneton@users.noreply.github.com>
2021-07-26 14:52:57 +0900

    UPLOAD

--- scripts/main.js
+++ scripts/main.js
@@ -13,16 +13,27 @@ window.ondrop = () => {
     event.stopPropagation();
     event.preventDefault();
     const files = event.target.files || event.dataTransfer.files;
-    for(let i = 0; i < files.length; i++) {
+
+    const sortResult = dataSort([...files], (a, b) => 
+        [a.name, b.name]
+    );
+
+    for(let i = 0; i < sortResult.length; i++) {
         fileEncode(files[i]);
     }
 };
 
 document.getElementsByClassName("select_encoding_file")[0].onchange = () => {
     const files = event.target.files || event.dataTransfer.files;
-    for(let i = 0; i < files.length; i++) {
+
+    const sortResult = dataSort([...files], (a, b) => 
+        [a.name, b.name]
+    );
+
+    for(let i = 0; i < sortResult.length; i++) {
         fileEncode(files[i]);
     }
+
     event.target.value = null;
 }
 
@@ -59,11 +70,18 @@ const fileEncode = file => {
     downloadBtn.setAttribute("class", "encode_item_download not_drag");
     downloadBtn.appendChild(loadBox());
 
+    const removeBtn = document.createElement("button");
+    removeBtn.setAttribute("class", "encode_item_remove not_drag");
+    removeBtn.appendChild(removeSvg());
+
     rightBox.appendChild(typeInfo);
+    rightBox.appendChild(removeBtn);
     rightBox.appendChild(downloadBtn);
 
     box.appendChild(fileName);
     box.appendChild(rightBox);
+
+    fileDirector.append(box);
 
     let url;
     encoding.start(file, {
@@ -89,6 +107,11 @@ const fileEncode = file => {
         }
     });
 
+    removeBtn.onclick = () => {
+        if(url != null) URL.revokeObjectURL(url);
+        box.remove();
+    };
+
     fileName.onclick = () => {
         if(url) {
             if(fileName.previewSrc != null) window.open(fileName.previewSrc);
@@ -96,19 +119,11 @@ const fileEncode = file => {
                 window.open(fileName.previewSrc = URL.createObjectURL(new Blob(["\ufeff", blob], { type: "text/plain" })));
             });
         }
-    }
+    };
 
     downloadBtn.onclick = () => {
         if(url) download(url, fileName.innerText);
-    }
-
-    const sortResult = dataSort([...fileDirector.children, box], (a, b) => 
-        [a.getElementsByClassName("encode_item_fileName")[0].innerText, b.getElementsByClassName("encode_item_fileName")[0].innerText]
-    );
-
-    for(let i = 0; i < sortResult.length; i++) {
-        fileDirector.appendChild(sortResult[i]);
-    }
+    };
 };
 
 const loadBox = () => {
@@ -121,14 +136,26 @@ const downloadSvg = () => {
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     svg.setAttribute("class", "download_svg");
     svg.setAttribute("viewBox", "0 0 512 512");
-    const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path1.setAttribute("d", "M382.56,233.376C379.968,227.648,374.272,224,368,224h-64V16c0-8.832-7.168-16-16-16h-64c-8.832,0-16,7.168-16,16v208h-64 c-6.272,0-11.968,3.68-14.56,9.376c-2.624,5.728-1.6,12.416,2.528,17.152l112,128c3.04,3.488,7.424,5.472,12.032,5.472 c4.608,0,8.992-2.016,12.032-5.472l112-128C384.192,245.824,385.152,239.104,382.56,233.376z");
-    const path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    path2.setAttribute("d", "M432,352v96H80v-96H16v128c0,17.696,14.336,32,32,32h416c17.696,0,32-14.304,32-32V352H432z");
-    svg.appendChild(path1);
-    svg.appendChild(path2);
+    svg.appendChild(createPath("M382.56,233.376C379.968,227.648,374.272,224,368,224h-64V16c0-8.832-7.168-16-16-16h-64c-8.832,0-16,7.168-16,16v208h-64 c-6.272,0-11.968,3.68-14.56,9.376c-2.624,5.728-1.6,12.416,2.528,17.152l112,128c3.04,3.488,7.424,5.472,12.032,5.472 c4.608,0,8.992-2.016,12.032-5.472l112-128C384.192,245.824,385.152,239.104,382.56,233.376z"));
+    svg.appendChild(createPath("M432,352v96H80v-96H16v128c0,17.696,14.336,32,32,32h416c17.696,0,32-14.304,32-32V352H432z"));
     return svg;
 };
+
+const removeSvg = () => {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("class", "remove_svg");
+    svg.setAttribute("viewBox", "0 0 512 512");
+    svg.appendChild(createPath("m240 240h30v165h-30z"));
+    svg.appendChild(createPath("m240 240h30v165h-30z"));
+    svg.appendChild(createPath("m450 60h-120v-15c0-24.813-20.186-45-45-45h-60c-24.814 0-45 20.187-45 45v15h-120v90h31.248l27.584 317.243c1.191 24.007 20.863 42.757 44.944 42.757h182.447c24.081 0 43.753-18.75 44.944-42.757l27.585-317.243h31.248zm-240-15c0-8.271 6.729-15 15-15h60c8.272 0 15 6.729 15 15v15h-90zm-120 45h330v30h-330zm271.211 375.624c-.336 8.061-6.919 14.376-14.987 14.376h-182.448c-8.068 0-14.651-6.314-14.987-14.376-29.348-337.707-27.341-314.616-27.429-315.624h267.28c-.08.905 1.788-20.569-27.429 315.624z"));
+    return svg;
+}
+
+const createPath = str => {
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", str);
+    return path;
+}
 
 const download = (url, name="name.txt") => {
     const link = document.createElement("a");
