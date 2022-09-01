8efb47eb2c4c37c05707863fddf025ecf7d64b32
skyneton <donginbae30@gmail.com>
2022-09-01 20:32:20 +0900

    update filtering

--- scripts/encoding.js
+++ scripts/encoding.js
@@ -167,7 +167,7 @@ function Encoding() {
         }
 
         types() {
-            return ["UTF-8", "IBM866", "Big5", "Shift_JIS", "EUC-KR", "EUC-JP", "ISO-2022-JP", "UTF-16BE", "UTF-16LE", "GBK", "gb18030", "ISO-8859-2", "ISO-8859-3", "ISO-8859-4", "ISO-8859-5", "ISO-8859-6", "ISO-8857-7", "ISO-8859-8", "ISO-8859-8-I", "ISO-8859-10", "ISO-8859-13", "ISO-8859-14", "ISO-8859-15", "ISO-8859-16", "KOI8-R", "KOI8-U", "macintosh", "windows-874", "windows-1250", "windows-1251", "windows-1252", "windows-1253", "windows-1254", "windows-1255", "windows-1256", "windows-1257", "windows-1258", "x-mac-cyrillic"];
+            return ["UTF-8", "EUC-KR", "Shift_JIS", "EUC-JP", "ISO-2022-JP", "UTF-16BE", "UTF-16LE", "GBK", "gb18030", "ISO-8859-2", "ISO-8859-3", "ISO-8859-4", "ISO-8859-5", "ISO-8859-6", "ISO-8857-7", "ISO-8859-8", "ISO-8859-8-I", "ISO-8859-10", "ISO-8859-13", "ISO-8859-14", "ISO-8859-15", "ISO-8859-16", "KOI8-R", "KOI8-U", "macintosh", "windows-874", "windows-1250", "windows-1251", "windows-1252", "windows-1253", "windows-1254", "windows-1255", "windows-1256", "windows-1257", "windows-1258", "x-mac-cyrillic", "IBM866", "Big5"];
         }
     }
 }
